92c8df46909023bad367dee250f2d02651f4aef1
Krzysztof Nyrek <nitrobenzen@gmail.com>
2019-12-15 15:41:12 +0100

    Added content to html table

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 "use strict";
+const table = document.querySelector('.table--js');
 fetch('https://api.github.com/users/dekstryn/repos?sort=updates')
 .then(resp => resp.json())
 .then(resp => {
-    console.log(resp)
+    const repos = resp;
+    for (const repo of repos) {
+        table.innerHTML += '<tr><th>' + repo.name + '</th><th>' + repo.description + '</th><th>' + repo.html_url + '</th><th>' + repo.created_at + '</th><th>' + repo.updated_at + '</th></tr>';
+    }
 })
 .catch(err =>{
-    console(err);
+    console.log(err);
 })
+
+function newFunction() {
+    '<th>';
+}
